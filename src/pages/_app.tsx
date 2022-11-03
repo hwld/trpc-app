4d62b30a479c59b14ec4f8ad1e7cea080e83b736
hwld <0701hw@gmail.com>
2022-11-03 23:34:03 +0900

    Add: next-authのインストール

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { trpc } from "../client/trpc";
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
       <Head>
@@ -14,11 +15,13 @@ function App({ Component, pageProps }: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <MantineProvider withGlobalStyles withNormalizeCSS theme={{}}>
-        <NotificationsProvider position="bottom-center">
-          <Component {...pageProps} />
-        </NotificationsProvider>
-      </MantineProvider>
+      <SessionProvider session={session}>
+        <MantineProvider withGlobalStyles withNormalizeCSS theme={{}}>
+          <NotificationsProvider position="bottom-center">
+            <Component {...pageProps} />
+          </NotificationsProvider>
+        </MantineProvider>
+      </SessionProvider>
     </>
   );
 }
